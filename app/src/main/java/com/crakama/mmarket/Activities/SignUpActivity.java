adc43f94356bf218910bc978b73a972deee7d2ae
crakama <crakama89@gmail.com>
2016-11-09 22:25:48 +0300

    Sign up/Login screen

---
+++ app/src/main/java/com/crakama/mmarket/Activities/SignUpActivity.java
@@ -0,0 +1,16 @@
+package com.crakama.mmarket.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.crakama.mmarket.R;
+
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
